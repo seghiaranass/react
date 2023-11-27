d7e580877347e9bf7ad6bc80c78cce473a48055d
Anass Seghiar <anass.segh97@gmail.com>
2023-11-27 09:51:37 +0100

    Manging State & Using Hooks [Core Concept]

--- REACT-ESSENTIALS/src/App.jsx
+++ REACT-ESSENTIALS/src/App.jsx
@@ -1,4 +1,4 @@
-import componentsImg from './assets/components.png'
+import {useState } from 'react'
 
 import { CORE_CONCEPTS } from './data';
 
@@ -10,12 +10,14 @@ import TabButton from './components/TabButton.jsx';
 
 
 function App() {
-  
+  const [seletedTopic,setSelectedTopic] = useState('Please click a button');
+  console.log("Ex")
   let tabContent  = 'Please click a button';
 
   function handleClick(selectedButton){
     // selectedButton => 'componenets', 'jsx', 'props', 'state'
     tabContent = selectedButton
+    setSelectedTopic(selectedButton)
     console.log(tabContent)
    }
   return (
@@ -30,15 +32,11 @@ function App() {
                 description={CORE_CONCEPTS[0].description}
                 image={CORE_CONCEPTS[0].image}
                />   
-
               <CoreConcept {...CORE_CONCEPTS[1]}/>   
 
               <CoreConcept {...CORE_CONCEPTS[2]}/>   
 
               <CoreConcept {...CORE_CONCEPTS[3]}/>   
-
-
-
             </ul>
         </section>
         <section id="examples">
@@ -50,7 +48,7 @@ function App() {
             <TabButton onSelect={() => handleClick('state')}>State</TabButton>
           </menu>
             
-            {tabContent}
+            {seletedTopic}
 
           
         </section>
