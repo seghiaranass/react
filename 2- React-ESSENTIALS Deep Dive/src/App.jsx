aeec4d3c4c18b2fd53d4301629d2da5b311a6734
Anass Seghiar <anass.segh97@gmail.com>
2023-11-30 15:33:01 +0100

    Before using forwarded props

--- 2- React-ESSENTIALS Deep Dive/src/App.jsx
+++ 2- React-ESSENTIALS Deep Dive/src/App.jsx
@@ -4,14 +4,6 @@ import CoreConcepts from "./components/CoreConcepts.jsx";
 import Examples from "./components/Examples.jsx";
 
 function App() {
-
-
-  console.log("APP COMPONENT EXECUTING");
-
-  
-
-
-
   return (
       <>
       <Header />
--- 2- React-ESSENTIALS Deep Dive/src/components/Examples.jsx
+++ 2- React-ESSENTIALS Deep Dive/src/components/Examples.jsx
@@ -1,59 +1,59 @@
 import { useState } from "react";
-import TabButton from "./TabButton"
+import TabButton from "./TabButton";
+import Section from "./Section.jsx";
 import { EXAMPLES } from "../data.js";
 
-export default function Examples(){
-    const [selectedTopic, setSelectedTopic] = useState();
-    let tabContent = <p>Please select a topic.</p>;
+export default function Examples() {
+  const [selectedTopic, setSelectedTopic] = useState();
+  let tabContent = <p>Please select a topic.</p>;
 
-    function handleSelect(selectedButton) {
-      // selectedButton => 'components', 'jsx', 'props', 'state'
-      setSelectedTopic(selectedButton);
-      // console.log(selectedTopic);
-    }
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+    setSelectedTopic(selectedButton);
+    // console.log(selectedTopic);
+  }
 
-    if (selectedTopic) {
-        tabContent = (
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
-        );
-      }
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
 
-    return (
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton
-              isSelected={selectedTopic === "components"}
-              onSelect={() => handleSelect("components")}
-            >
-              Components
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "jsx"}
-              onSelect={() => handleSelect("jsx")}
-            >
-              JSX
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "props"}
-              onSelect={() => handleSelect("props")}
-            >
-              Props
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "state"}
-              onSelect={() => handleSelect("state")}
-            >
-              State
-            </TabButton>
-          </menu>
-          {tabContent}
-        </section>
-    )
+  return (
+    <Section id="examples" title="Examples">
+      <menu>
+        <TabButton
+          isSelected={selectedTopic === "components"}
+          onSelect={() => handleSelect("components")}
+        >
+          Components
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "jsx"}
+          onSelect={() => handleSelect("jsx")}
+        >
+          JSX
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "props"}
+          onSelect={() => handleSelect("props")}
+        >
+          Props
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "state"}
+          onSelect={() => handleSelect("state")}
+        >
+          State
+        </TabButton>
+      </menu>
+      {tabContent}
+    </Section>
+  );
 }
